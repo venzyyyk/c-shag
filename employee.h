3e2a32e7ef272093485a82fcf2d5473ddbb6ba97
venzyyyk <cubeday2008@gmail.com>
2024-06-28 14:42:28 +0300

    Add files via upload

---
+++ employee.h
@@ -0,0 +1,14 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <string>
+
+struct Employee {
+    std::string surname;
+    int birthYear;
+    std::string position;
+    double salary;
+    std::string education;
+};
+
+#endif
